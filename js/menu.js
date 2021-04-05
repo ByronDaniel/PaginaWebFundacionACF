4d538dd746ecd39f095c4cd5a402053021cc715f
BDSystems-Ecuador <byrondanipm@gmail.com>
2021-04-05 16:42:41 -0500

    pantalla principal

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,24 @@
+//Menús
 var btnmenu = document.getElementById("btnmenu");
 var menu = document.getElementById("menu");
+var btnInicio = document.getElementById("btnInicio");
+var menu2 = document.getElementById("menu2");
 
 btnmenu.addEventListener('click',()=>{
     menu.classList.toggle('nav-show');
 });
 
-var btnInicio = document.getElementById("btnInicio");
-var menu2 = document.getElementById("menu2");
-
-btnInicio.addEventListener('click',()=>{
-    menu2.classList.toggle('nav-show2');
+//Slider con Glider js
+window.addEventListener('load',function(){
+    new Glider(document.querySelector('.carousel__lista'),{
+            slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    autoplay:2000,
+    dots: '.carousel__indicadores',
+    arrows: {
+        prev: '.carousel__anterior',
+        next: '.carousel__siguiente'
+    }}
+    );
 });
